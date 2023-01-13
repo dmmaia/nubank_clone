9fa447e7faf322f61d8e3c514c43647fb0a7b68b
dmmaia <david_mmaia@hotmail.com>
2023-01-13 11:16:02 -0300

    style(): some icons and style added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:nubank_clone/views/home_screen.dart';
 
 void main() {
@@ -8,24 +9,18 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Nubank Clone',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: HomeScreen(),
+      home: const AnnotatedRegion<SystemUiOverlayStyle>(
+          value: SystemUiOverlayStyle(
+            statusBarColor: Colors.transparent,
+          ),
+          child: HomeScreen()),
     );
   }
 }
--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -8,12 +8,71 @@ class HomeScreen extends StatelessWidget {
     return SingleChildScrollView(
       child: Column(children: [
         Container(
+          padding: const EdgeInsetsDirectional.fromSTEB(20, 20, 20, 0),
           height: 200,
-          color: Colors.deepPurple,
+          color: Colors.purple.shade800,
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      width: 50,
+                      height: 50,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(25),
+                        color: Colors.purple.shade400,
+                      ),
+                      child: const Icon(
+                        Icons.person_outline,
+                        color: Colors.white,
+                        size: 30.0,
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        Container(
+                          padding:
+                              const EdgeInsetsDirectional.fromSTEB(0, 0, 20, 0),
+                          child: const Icon(
+                            Icons.visibility_outlined,
+                            color: Colors.white,
+                            size: 28.0,
+                          ),
+                        ),
+                        Container(
+                          padding:
+                              const EdgeInsetsDirectional.fromSTEB(0, 0, 20, 0),
+                          child: const Icon(
+                            Icons.help_outline,
+                            color: Colors.white,
+                            size: 28.0,
+                          ),
+                        ),
+                        Container(
+                            child: const Icon(
+                          Icons.mail_outlined,
+                          color: Colors.white,
+                          size: 28.0,
+                        )),
+                      ],
+                    )
+                  ],
+                ),
+                const Text(
+                  "Olá, David",
+                  style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.white,
+                      decoration: TextDecoration.none),
+                ),
+              ]),
         ),
         Container(
           padding: const EdgeInsetsDirectional.fromSTEB(20, 0, 20, 0),
-          height: 300,
+          height: 450,
           color: Colors.white,
           child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
@@ -21,15 +80,28 @@ class HomeScreen extends StatelessWidget {
               children: [
                 Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
-                  children: const [
-                    Text(
-                      "Conta",
-                      style: TextStyle(
-                          fontSize: 20,
-                          color: Colors.black,
-                          decoration: TextDecoration.none),
-                    ),
-                    Text(
+                  children: [
+                    Container(
+                        padding:
+                            const EdgeInsetsDirectional.fromSTEB(0, 0, 0, 20),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            const Text(
+                              "Conta",
+                              style: TextStyle(
+                                  fontSize: 20,
+                                  color: Colors.black,
+                                  decoration: TextDecoration.none),
+                            ),
+                            Icon(
+                              Icons.chevron_right,
+                              color: Colors.grey.shade500,
+                              size: 30.0,
+                            ),
+                          ],
+                        )),
+                    const Text(
                       "R\$ 1.000,00",
                       style: TextStyle(
                           fontSize: 22,
@@ -42,8 +114,22 @@ class HomeScreen extends StatelessWidget {
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     Column(
-                      children: const [
-                        Text(
+                      children: [
+                        Container(
+                          width: 70,
+                          height: 70,
+                          margin: const EdgeInsets.only(bottom: 10),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(35),
+                            color: Colors.grey.shade200,
+                          ),
+                          child: const Icon(
+                            Icons.pix,
+                            color: Colors.black,
+                            size: 30.0,
+                          ),
+                        ),
+                        const Text(
                           "Pix",
                           style: TextStyle(
                               fontSize: 14,
@@ -53,8 +139,22 @@ class HomeScreen extends StatelessWidget {
                       ],
                     ),
                     Column(
-                      children: const [
-                        Text(
+                      children: [
+                        Container(
+                          width: 70,
+                          height: 70,
+                          margin: const EdgeInsets.only(bottom: 10),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(35),
+                            color: Colors.grey.shade200,
+                          ),
+                          child: const Icon(
+                            Icons.view_column,
+                            color: Colors.black,
+                            size: 30.0,
+                          ),
+                        ),
+                        const Text(
                           "Pagar",
                           style: TextStyle(
                               fontSize: 14,
@@ -64,8 +164,22 @@ class HomeScreen extends StatelessWidget {
                       ],
                     ),
                     Column(
-                      children: const [
-                        Text(
+                      children: [
+                        Container(
+                          width: 70,
+                          height: 70,
+                          margin: const EdgeInsets.only(bottom: 10),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(35),
+                            color: Colors.grey.shade200,
+                          ),
+                          child: const Icon(
+                            Icons.pix,
+                            color: Colors.black,
+                            size: 30.0,
+                          ),
+                        ),
+                        const Text(
                           "Transferir",
                           style: TextStyle(
                               fontSize: 14,
@@ -75,8 +189,22 @@ class HomeScreen extends StatelessWidget {
                       ],
                     ),
                     Column(
-                      children: const [
-                        Text(
+                      children: [
+                        Container(
+                          width: 70,
+                          height: 70,
+                          margin: const EdgeInsets.only(bottom: 10),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(35),
+                            color: Colors.grey.shade200,
+                          ),
+                          child: const Icon(
+                            Icons.pix,
+                            color: Colors.black,
+                            size: 30.0,
+                          ),
+                        ),
+                        const Text(
                           "Depositar",
                           style: TextStyle(
                               fontSize: 14,
@@ -89,13 +217,21 @@ class HomeScreen extends StatelessWidget {
                 ),
                 Container(
                   width: 400,
-                  height: 50,
+                  height: 60,
                   decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
+                    borderRadius: BorderRadius.circular(10),
                     color: Colors.grey.shade200,
                   ),
                   child: Row(
                     children: const [
+                      Padding(
+                        padding: EdgeInsetsDirectional.fromSTEB(20, 0, 20, 0),
+                        child: Icon(
+                          Icons.credit_card,
+                          color: Colors.black,
+                          size: 30.0,
+                        ),
+                      ),
                       Text(
                         "Meus cartões",
                         style: TextStyle(
@@ -105,9 +241,37 @@ class HomeScreen extends StatelessWidget {
                       )
                     ],
                   ),
+                ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      width: 300,
+                      height: 80,
+                      padding:
+                          const EdgeInsetsDirectional.fromSTEB(20, 0, 20, 0),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Colors.grey.shade200,
+                      ),
+                      child: Row(
+                        children: [
+                          Flexible(
+                              child: Text(
+                            "Você tem R\$ 25.000,00 disponíveis para empréstimo.",
+                            overflow: TextOverflow.visible,
+                            style: TextStyle(
+                                fontSize: 15,
+                                color: Colors.grey[800],
+                                decoration: TextDecoration.none),
+                          ))
+                        ],
+                      ),
+                    )
+                  ],
                 )
               ]),
-        )
+        ),
       ]),
     );
   }
